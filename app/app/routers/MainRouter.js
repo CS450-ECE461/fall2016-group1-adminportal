6a22a84916301948927201a43912364120efb1dd
Kyle Fetterman <kfetterm@iupui.edu>
2016-11-01 19:56:41 -0400

    Added create organization page

--- app/app/routers/MainRouter.js
+++ app/app/routers/MainRouter.js
@@ -1,9 +1,15 @@
 module.exports = exports = {
-  '/helloworld' : {
-    get  : { view   : 'helloworld.pug' },
-    post : { action : 'HelloWorldController@echoName'},
-  },
+  // '/helloworld' : {
+  //   get  : { view   : 'helloworld.pug' },
+  //   post : { action : 'HelloWorldController@echoName'},
+  // },
+  
   '/createOrganization' : {
-  	get : {view:'CreateOrganization.pug'}
-  }
+  	get : {view:'CreateOrganization.pug' },
+  	post: {action:'CreateOrganizationController@echoOrganization'},
+  },
+
+  // '/adminLogin' : {
+  // 	get :{ view: 'adminLogin.handlebars'}
+  // }
 };
