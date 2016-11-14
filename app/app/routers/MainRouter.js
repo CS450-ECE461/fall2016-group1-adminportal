3be1b319ec3081746497cf8de58c8cb910b37c00
ScottGravesCE <gravess@umail.iu.edu>
2016-11-14 08:09:30 -0500

    Update MainRouter.js

--- app/app/routers/MainRouter.js
+++ app/app/routers/MainRouter.js
@@ -1,9 +1,5 @@
 module.exports = exports = {
-  // '/helloworld' : {
-  //   get  : { view   : 'helloworld.pug' },
-  //   post : { action : 'HelloWorldController@echoName'},
-  // },
-
+  
   '/createOrganization' : {
   	get: {view: 'CreateOrganization.pug'},
   	post: {action:'CreateOrganizationController@echoOrganization'},
